19322394335c0c8aa5aabd2994d5a0d70bb82fcd
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2023-10-11 12:48:21 +0100

    Apollo Client Working

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+  uri: "http://localhost:5000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetAutomations {
+        allAutomations {
+          id
+          title
+          description
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+);
